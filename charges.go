a9d4c238dfdee13c69c5c59fee2c3956cd340fb2
Brooke E Kline Jr <brooke.kline@moov.io>
2019-05-10 16:29:25 -0400

    add charges validation function
    add charges validation function

--- charges.go
+++ charges.go
@@ -83,27 +83,27 @@ func (c *Charges) Validate() error {
 	if err := c.isAlphanumeric(c.SendersChargesOne); err != nil {
 		return fieldError("SendersChargesOne", err, c.SendersChargesOne)
 	}
-	/*	if err := c.isChargesValid(c.SendersChargesOne); err != nil {
+	if err := c.isChargesValid(c.SendersChargesOne); err != nil {
 		return fieldError("SendersChargesOne", err, c.SendersChargesOne)
-	}*/
+	}
 	if err := c.isAlphanumeric(c.SendersChargesTwo); err != nil {
 		return fieldError("SendersChargesTwo", err, c.SendersChargesTwo)
 	}
-	/*	if err := c.isChargesValid(c.SendersChargesTwo); err != nil {
+	if err := c.isChargesValid(c.SendersChargesTwo); err != nil {
 		return fieldError("SendersChargesTwo", err, c.SendersChargesTwo)
-	}*/
+	}
 	if err := c.isAlphanumeric(c.SendersChargesThree); err != nil {
 		return fieldError("SendersChargesThree", err, c.SendersChargesThree)
 	}
-	/*	if err := c.isChargesValid(c.SendersChargesThree); err != nil {
+	if err := c.isChargesValid(c.SendersChargesThree); err != nil {
 		return fieldError("SendersChargesThree", err, c.SendersChargesThree)
-	}*/
+	}
 	if err := c.isAlphanumeric(c.SendersChargesFour); err != nil {
 		return fieldError("SendersChargesFour", err, c.SendersChargesFour)
 	}
-	/*	if err := c.isChargesValid(c.SendersChargesFour); err != nil {
+	if err := c.isChargesValid(c.SendersChargesFour); err != nil {
 		return fieldError("SendersChargesFour", err, c.SendersChargesFour)
-	}*/
+	}
 	return nil
 }
 
@@ -138,7 +138,7 @@ func (c *Charges) SendersChargesFourField() string {
 	return c.alphaField(c.SendersChargesFour, 15)
 }
 
-/*
+
 func (c *Charges) isChargesValid(s string) error {
 	if s == "" {
 		return nil
@@ -152,4 +152,4 @@ func (c *Charges) isChargesValid(s string) error {
 		return fieldError("Amount", err)
 	}
 	return nil
-}*/
+}
