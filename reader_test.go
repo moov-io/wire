ec6c615b8302274375fa67ee94e792495ba71eed
Brooke E Kline Jr <brooke.kline@moov.io>
2019-06-04 14:26:14 -0400

    openapi
    Test updates
    Captured static check results, so commented for the time being

--- reader_test.go
+++ reader_test.go
@@ -3,25 +3,31 @@ package wire
 import (
 	"github.com/moov-io/base"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
+
+
+
 // TestRead reads wire Files with different BusinessFunctionCodes
 func TestRead(t *testing.T) {
-	t.Run("BankTransfer", testRead("./test/testdata/fedWireMessage-BankTransfer.txt"))
-	t.Run("CustomerTransfer", testRead("./test/testdata/fedWireMessage-CustomerTransfer.txt"))
-	t.Run("CustomerTransferPlus", testRead("./test/testdata/fedWireMessage-CustomerTransferPlus.txt"))
-	t.Run("CheckSameDaySettlement", testRead("./test/testdata/fedWireMessage-CheckSameDaySettlement.txt"))
-	t.Run("DepositSendersAccount", testRead("./test/testdata/fedWireMessage-DepositSendersAccount.txt"))
-	t.Run("FEDFundsReturned", testRead("./test/testdata/fedWireMessage-FEDFundsReturned.txt"))
-	t.Run("FEDFundsSold", testRead("./test/testdata/fedWireMessage-FEDFundsSold.txt"))
-	t.Run("DrawdownRequest", testRead("./test/testdata/fedWireMessage-DrawdownRequest.txt"))
-	t.Run("BankDrawdownRequest", testRead("./test/testdata/fedWireMessage-BankDrawdownRequest.txt"))
-	t.Run("CustomerCorporateDrawdownRequest", testRead("./test/testdata/fedWireMessage-CustomerCorporateDrawdownRequest.txt"))
-	t.Run("ServiceMessage", testRead("./test/testdata/fedWireMessage-ServiceMessage.txt"))
-	t.Run("CustomerTransferPlusCOVS", testRead("./test/testdata/fedWireMessage-CustomerTransferPlusCOVS.txt"))
-	t.Run("CustomerTransferPlusUnstructuredAddenda", testRead("./test/testdata/fedWireMessage-CustomerTransferPlusUnstructuredAddenda.txt"))
-	t.Run("CustomerTransferPlusStructuredRemittance", testRead("./test/testdata/fedWireMessage-CustomerTransferPlusStructuredRemittance.txt"))
+	//t.Run("BankTransfer", testRead("./test/testdata/fedWireMessage-BankTransfer.txt"))
+
+	t.Run("BankTransfer", testRead(filepath.Join("test", "testdata", "fedWireMessage-BankTransfer.txt")))
+	t.Run("CustomerTransfer", testRead(filepath.Join("test", "testdata", "fedWireMessage-CustomerTransfer.txt")))
+	t.Run("CustomerTransferPlus", testRead(filepath.Join("test", "testdata","fedWireMessage-CustomerTransferPlus.txt")))
+	t.Run("CheckSameDaySettlement", testRead(filepath.Join("test", "testdata","fedWireMessage-CheckSameDaySettlement.txt")))
+	t.Run("DepositSendersAccount", testRead(filepath.Join("test", "testdata","fedWireMessage-DepositSendersAccount.txt")))
+	t.Run("FEDFundsReturned", testRead(filepath.Join("test", "testdata","fedWireMessage-FEDFundsReturned.txt")))
+	t.Run("FEDFundsSold", testRead(filepath.Join("test", "testdata","fedWireMessage-FEDFundsSold.txt")))
+	t.Run("DrawdownRequest", testRead(filepath.Join("test", "testdata","fedWireMessage-DrawdownRequest.txt")))
+	t.Run("BankDrawdownRequest", testRead(filepath.Join("test", "testdata","fedWireMessage-BankDrawdownRequest.txt")))
+	t.Run("CustomerCorporateDrawdownRequest", testRead(filepath.Join("test", "testdata","fedWireMessage-CustomerCorporateDrawdownRequest.txt")))
+	t.Run("ServiceMessage", testRead(filepath.Join("test", "testdata","fedWireMessage-ServiceMessage.txt")))
+	t.Run("CustomerTransferPlusCOVS", testRead(filepath.Join("test", "testdata","fedWireMessage-CustomerTransferPlusCOVS.txt")))
+	t.Run("CustomerTransferPlusUnstructuredAddenda", testRead(filepath.Join("test", "testdata","fedWireMessage-CustomerTransferPlusUnstructuredAddenda.txt")))
+	t.Run("CustomerTransferPlusStructuredRemittance", testRead(filepath.Join("test", "testdata","fedWireMessage-CustomerTransferPlusStructuredRemittance.txt")))
 }
 
 func testRead(filePathName string) func(t *testing.T) {
