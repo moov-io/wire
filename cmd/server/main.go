5b0ab6abedeffe7a837915ab1dee8a37b0ba9c4f
Brooke Kline <brooke.kline@moov.io>
2019-06-06 19:33:20 -0400

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,8 +6,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/moov-io/wire"
 )
 
 func main() {
-	fmt.Printf("Starting moov-io/wire version %s\n", "v-0.1.0-dev")
+	fmt.Printf("Starting moov-io/wire version %s\n", wire.Version)
 }
