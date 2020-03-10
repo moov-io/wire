246021374312139f22881cd5b8726ed799fb73b3
Adam Shannon <adamkshannon@gmail.com>
2020-03-10 13:22:23 -0500

    server: add version handler to admin HTTP server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -57,6 +57,7 @@ func main() {
 
 	// Start Admin server (with Prometheus metrics)
 	adminServer := admin.NewServer(*adminAddr)
+	adminServer.AddVersionHandler(wire.Version) // Setup 'GET /version'
 	go func() {
 		logger.Log("admin", fmt.Sprintf("listening on %s", adminServer.BindAddr()))
 		if err := adminServer.Listen(); err != nil {
