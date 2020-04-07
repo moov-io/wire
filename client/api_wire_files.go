f596adaed405aa86818e8c7865363bd3b94ec75e
Adam Shannon <adamkshannon@gmail.com>
2020-04-06 20:37:10 -0500

    api,client: use shorter API summaries

--- client/api_wire_files.go
+++ client/api_wire_files.go
@@ -35,6 +35,7 @@ type AddFEDWireMessageToFileOpts struct {
 
 /*
 AddFEDWireMessageToFile Add FEDWireMessage to File
+Add a FEDWireMessage to the specified file
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param fileID File ID
  * @param fedWireMessage
@@ -118,7 +119,8 @@ type CreateWireFileOpts struct {
 }
 
 /*
-CreateWireFile Create a new File object
+CreateWireFile Create File
+Create a new File object from either the plaintext or JSON representation.
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param createWireFile Content of the WIRE file (in json or raw text)
  * @param optional nil or *CreateWireFileOpts - Optional Parameters:
@@ -229,7 +231,8 @@ type DeleteWireFileByIDOpts struct {
 }
 
 /*
-DeleteWireFileByID Permanently deletes a File and associated FEDWireMessage. It cannot be undone.
+DeleteWireFileByID Delete file
+Permanently deletes a File and associated Batches. It cannot be undone.
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param fileID File ID
  * @param optional nil or *DeleteWireFileByIDOpts - Optional Parameters:
@@ -305,7 +308,8 @@ type GetWireFileByIDOpts struct {
 }
 
 /*
-GetWireFileByID Retrieves the details of an existing File. You need only supply the unique File identifier that was returned upon creation.
+GetWireFileByID Retrieve a file
+Get the details of an existing File using the unique File identifier that was returned upon creation.
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param fileID File ID
  * @param optional nil or *GetWireFileByIDOpts - Optional Parameters:
@@ -402,7 +406,8 @@ type GetWireFileContentsOpts struct {
 }
 
 /*
-GetWireFileContents Assembles the existing file with a FEDWireMessage, Returns plaintext file.
+GetWireFileContents Get file contents
+Assembles the existing file, computes sequence numbers and totals. Returns plaintext file.
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param fileID File ID
  * @param optional nil or *GetWireFileContentsOpts - Optional Parameters:
@@ -498,7 +503,8 @@ type GetWireFilesOpts struct {
 }
 
 /*
-GetWireFiles Gets a list of Files
+GetWireFiles Get files
+List all Wire files created with the Wire service. These files are not persisted through multiple runs of the service.
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param optional nil or *GetWireFilesOpts - Optional Parameters:
  * @param "XRequestID" (optional.String) -  Optional Request ID allows application developer to trace requests through the systems logs
@@ -587,7 +593,8 @@ func (a *WireFilesApiService) GetWireFiles(ctx _context.Context, localVarOptiona
 }
 
 /*
-Ping Ping the Wire service to check if running
+Ping Ping Wire
+Check the Wire service is running
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 */
 func (a *WireFilesApiService) Ping(ctx _context.Context) (*_nethttp.Response, error) {
@@ -650,127 +657,14 @@ func (a *WireFilesApiService) Ping(ctx _context.Context) (*_nethttp.Response, er
 	return localVarHTTPResponse, nil
 }
 
-// UpdateWireFileByIDOpts Optional parameters for the method 'UpdateWireFileByID'
-type UpdateWireFileByIDOpts struct {
-	XRequestID      optional.String
-	XIdempotencyKey optional.String
-}
-
-/*
-UpdateWireFileByID Updates the specified FEDWire Message by setting the values of the parameters passed. Any parameters not provided will be left unchanged.
- * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
- * @param fileID File ID
- * @param createWireFile
- * @param optional nil or *UpdateWireFileByIDOpts - Optional Parameters:
- * @param "XRequestID" (optional.String) -  Optional Request ID allows application developer to trace requests through the systems logs
- * @param "XIdempotencyKey" (optional.String) -  Idempotent key in the header which expires after 24 hours. These strings should contain enough entropy for to not collide with each other in your requests.
-@return WireFile
-*/
-func (a *WireFilesApiService) UpdateWireFileByID(ctx _context.Context, fileID string, createWireFile CreateWireFile, localVarOptionals *UpdateWireFileByIDOpts) (WireFile, *_nethttp.Response, error) {
-	var (
-		localVarHTTPMethod   = _nethttp.MethodPost
-		localVarPostBody     interface{}
-		localVarFormFileName string
-		localVarFileName     string
-		localVarFileBytes    []byte
-		localVarReturnValue  WireFile
-	)
-
-	// create path and map variables
-	localVarPath := a.client.cfg.BasePath + "/files/{fileID}"
-	localVarPath = strings.Replace(localVarPath, "{"+"fileID"+"}", _neturl.QueryEscape(fmt.Sprintf("%v", fileID)), -1)
-
-	localVarHeaderParams := make(map[string]string)
-	localVarQueryParams := _neturl.Values{}
-	localVarFormParams := _neturl.Values{}
-
-	// to determine the Content-Type header
-	localVarHTTPContentTypes := []string{"application/json"}
-
-	// set Content-Type header
-	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
-	if localVarHTTPContentType != "" {
-		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
-	}
-
-	// to determine the Accept header
-	localVarHTTPHeaderAccepts := []string{"application/json"}
-
-	// set Accept header
-	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
-	if localVarHTTPHeaderAccept != "" {
-		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
-	}
-	if localVarOptionals != nil && localVarOptionals.XRequestID.IsSet() {
-		localVarHeaderParams["X-Request-ID"] = parameterToString(localVarOptionals.XRequestID.Value(), "")
-	}
-	if localVarOptionals != nil && localVarOptionals.XIdempotencyKey.IsSet() {
-		localVarHeaderParams["X-Idempotency-Key"] = parameterToString(localVarOptionals.XIdempotencyKey.Value(), "")
-	}
-	// body params
-	localVarPostBody = &createWireFile
-	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFormFileName, localVarFileName, localVarFileBytes)
-	if err != nil {
-		return localVarReturnValue, nil, err
-	}
-
-	localVarHTTPResponse, err := a.client.callAPI(r)
-	if err != nil || localVarHTTPResponse == nil {
-		return localVarReturnValue, localVarHTTPResponse, err
-	}
-
-	localVarBody, err := _ioutil.ReadAll(localVarHTTPResponse.Body)
-	localVarHTTPResponse.Body.Close()
-	if err != nil {
-		return localVarReturnValue, localVarHTTPResponse, err
-	}
-
-	if localVarHTTPResponse.StatusCode >= 300 {
-		newErr := GenericOpenAPIError{
-			body:  localVarBody,
-			error: localVarHTTPResponse.Status,
-		}
-		if localVarHTTPResponse.StatusCode == 201 {
-			var v WireFile
-			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
-			if err != nil {
-				newErr.error = err.Error()
-				return localVarReturnValue, localVarHTTPResponse, newErr
-			}
-			newErr.model = v
-			return localVarReturnValue, localVarHTTPResponse, newErr
-		}
-		if localVarHTTPResponse.StatusCode == 400 {
-			var v Error
-			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
-			if err != nil {
-				newErr.error = err.Error()
-				return localVarReturnValue, localVarHTTPResponse, newErr
-			}
-			newErr.model = v
-		}
-		return localVarReturnValue, localVarHTTPResponse, newErr
-	}
-
-	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
-	if err != nil {
-		newErr := GenericOpenAPIError{
-			body:  localVarBody,
-			error: err.Error(),
-		}
-		return localVarReturnValue, localVarHTTPResponse, newErr
-	}
-
-	return localVarReturnValue, localVarHTTPResponse, nil
-}
-
 // ValidateWireFileOpts Optional parameters for the method 'ValidateWireFile'
 type ValidateWireFileOpts struct {
 	XRequestID optional.String
 }
 
 /*
-ValidateWireFile Validates the existing file. You need only supply the unique File identifier that was returned upon creation.
+ValidateWireFile Validate file
+Validates the existing file. You need only supply the unique File identifier that was returned upon creation.
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param fileID File ID
  * @param optional nil or *ValidateWireFileOpts - Optional Parameters:
