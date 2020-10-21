f72490fd41a12523ffa564302183d8cd1a2d84cf
Daniel Tonks <daniel@moov.io>
2020-10-21 16:05:21 -0600

    AccountDebitedDrawdown: refactor unit tests

--- accountDebitedDrawdown_test.go
+++ accountDebitedDrawdown_test.go
@@ -1,9 +1,10 @@
 package wire
 
 import (
-	"github.com/moov-io/base"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // mockAccountDebitedDrawdown creates a AccountDebitedDrawdown
@@ -21,119 +22,123 @@ func mockAccountDebitedDrawdown() *AccountDebitedDrawdown {
 // TestMockAccountDebitedDrawdown validates mockAccountDebitedDrawdown
 func TestMockAccountDebitedDrawdown(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
-	if err := debitDD.Validate(); err != nil {
-		t.Error("mockAccountDebitedDrawdown does not validate and will break other tests")
-	}
+
+	require.NoError(t, debitDD.Validate(), "mockAccountDebitedDrawdown does not validate and will break other tests")
 }
 
 // TestADDIdentifierAlphaNumeric validates Name is alphanumeric
 func TestADDIdentifierAlphaNumeric(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.Identifier = "®"
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrNonAlphanumeric) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAlphanumeric.Error())
 }
 
 // TestADDNameAlphaNumeric validates Identifier is alphanumeric
 func TestADDNameAlphaNumeric(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.Name = "®"
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrNonAlphanumeric) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAlphanumeric.Error())
 }
 
 // TestADDAddressLineOneAlphaNumeric validates AddressLineOne is alphanumeric
 func TestADDAddressLineOneAlphaNumeric(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.Address.AddressLineOne = "®"
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrNonAlphanumeric) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAlphanumeric.Error())
 }
 
 // TestADDAddressLineTwoAlphaNumeric validates AddressLineTwo is alphanumeric
 func TestADDAddressLineTwoAlphaNumeric(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.Address.AddressLineTwo = "®"
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrNonAlphanumeric) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAlphanumeric.Error())
 }
 
 // TestADDAddressLineThreeAlphaNumeric validates AddressLineThree is alphanumeric
 func TestADDAddressLineThreeAlphaNumeric(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.Address.AddressLineThree = "®"
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrNonAlphanumeric) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAlphanumeric.Error())
 }
 
 // TestADDIdentifierRequired validates Identifier is required
 func TestADDIdentifierRequired(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.Identifier = ""
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrFieldRequired) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	expected := fieldError("Identifier", ErrFieldRequired).Error()
+	require.Equal(t, expected, err.Error())
 }
 
 // TestADDNameRequired validates Name is required
 func TestADDNameRequired(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.Name = ""
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrFieldRequired) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	expected := fieldError("Name", ErrFieldRequired).Error()
+	require.Equal(t, expected, err.Error())
 }
 
 // TestADDIdentificationRequired validates IdentificationCode is required
 func TestADDIdentificationCodeRequired(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.IdentificationCode = ""
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrFieldRequired) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	expected := fieldError("IdentificationCode", ErrFieldRequired).Error()
+	require.Equal(t, expected, err.Error())
 }
 
 // TestADDIdentificationCodeValid validates IdentificationCode
 func TestADDIdentificationCodeValid(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.IdentificationCode = TaxIdentificationNumber
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrIdentificationCode) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	expected := fieldError("IdentificationCode", ErrIdentificationCode, debitDD.IdentificationCode).Error()
+	require.Equal(t, expected, err.Error())
 }
 
 // TestADDIdentificationCodeBogus validates IdentificationCode if the IdentificationCode is bogus
 func TestIdentificationCodeBogus(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.IdentificationCode = "Card ID"
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrIdentificationCode) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	expected := fieldError("IdentificationCode", ErrIdentificationCode, debitDD.IdentificationCode).Error()
+	require.Equal(t, expected, err.Error())
 }
 
 // TestParseAccountDebitedDrawdownWrongLength parses a wrong AccountDebitedDrawdown record length
@@ -141,15 +146,12 @@ func TestParseAccountDebitedDrawdownWrongLength(t *testing.T) {
 	var line = "{4400}D123456789                         debitDD Name                       Address One                        Address Two                        Address Three                    "
 	r := NewReader(strings.NewReader(line))
 	r.line = line
-	fwm := new(FEDWireMessage)
-	debitDD := mockAccountDebitedDrawdown()
-	fwm.SetAccountDebitedDrawdown(debitDD)
+
 	err := r.parseAccountDebitedDrawdown()
-	if err != nil {
-		if !base.Match(err, NewTagWrongLengthErr(181, len(r.line))) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	require.NotNil(t, err)
+	expected := r.parseError(NewTagWrongLengthErr(181, len(r.line))).Error()
+	require.Equal(t, expected, err.Error())
 }
 
 // TestParseAccountDebitedDrawdownReaderParseError parses a wrong AccountDebitedDrawdown reader parse error
@@ -157,30 +159,34 @@ func TestParseAccountDebitedDrawdownReaderParseError(t *testing.T) {
 	var line = "{4400}D123456789                         debitDD ®ame                       Address One                        Address Two                        Address Three                      "
 	r := NewReader(strings.NewReader(line))
 	r.line = line
-	fwm := new(FEDWireMessage)
-	debitDD := mockAccountDebitedDrawdown()
-	fwm.SetAccountDebitedDrawdown(debitDD)
+
 	err := r.parseAccountDebitedDrawdown()
-	if err != nil {
-		if !base.Match(err, ErrNonAlphanumeric) {
-			t.Errorf("%T: %s", err, err)
-		}
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAlphanumeric.Error())
+
+	fwm := mockCustomerTransferData()
+	fwm.AccountDebitedDrawdown = &AccountDebitedDrawdown{}
+	if err := fwm.AccountDebitedDrawdown.Parse(line); err != nil {
+		t.Fatal(err)
 	}
+	fwm.Beneficiary = mockBeneficiary()
+	fwm.Originator = mockOriginator()
+	r.currentFEDWireMessage = fwm
+
 	_, err = r.Read()
-	if err != nil {
-		if !base.Has(err, ErrNonAlphanumeric) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAlphanumeric.Error())
 }
 
 // TestAccountDebitedDrawdownTagError validates AccountDebitedDrawdown tag
 func TestAccountDebitedDrawdownTagError(t *testing.T) {
 	debitDD := mockAccountDebitedDrawdown()
 	debitDD.tag = "{9999}"
-	if err := debitDD.Validate(); err != nil {
-		if !base.Match(err, ErrValidTagForType) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := debitDD.Validate()
+
+	require.NotNil(t, err)
+	require.Equal(t, fieldError("tag", ErrValidTagForType, debitDD.tag).Error(), err.Error())
 }
