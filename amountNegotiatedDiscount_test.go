5f6ed9c38fe20a69ae86f8228440056e78a6a03f
Daniel Tonks <daniel@moov.io>
2020-10-27 11:24:24 -0600

    amountNegotiatedDiscount: refactor unit tests

--- amountNegotiatedDiscount_test.go
+++ amountNegotiatedDiscount_test.go
@@ -1,9 +1,10 @@
 package wire
 
 import (
-	"github.com/moov-io/base"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // AmountNegotiatedDiscount creates a AmountNegotiatedDiscount
@@ -17,53 +18,52 @@ func mockAmountNegotiatedDiscount() *AmountNegotiatedDiscount {
 // TestMockAmountNegotiatedDiscount validates mockAmountNegotiatedDiscount
 func TestMockAmountNegotiatedDiscount(t *testing.T) {
 	nd := mockAmountNegotiatedDiscount()
-	if err := nd.Validate(); err != nil {
-		t.Error("mockAmountNegotiatedDiscount does not validate and will break other tests")
-	}
+
+	require.NoError(t, nd.Validate(), "mockAmountNegotiatedDiscount does not validate and will break other tests")
 }
 
 // TestAmountNegotiatedDiscountAmountValid validates AmountNegotiatedDiscount Amount
 func TestAmountNegotiatedDiscountValid(t *testing.T) {
 	nd := mockAmountNegotiatedDiscount()
 	nd.RemittanceAmount.Amount = "X,"
-	if err := nd.Validate(); err != nil {
-		if !base.Match(err, ErrNonAmount) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := nd.Validate()
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAmount.Error())
 }
 
 // TestAmountNegotiatedDiscountCurrencyCodeValid validates AmountNegotiatedDiscount CurrencyCode
 func TestAmountNegotiatedDiscountCurrencyCodeValid(t *testing.T) {
 	nd := mockAmountNegotiatedDiscount()
 	nd.RemittanceAmount.CurrencyCode = "XZP"
-	if err := nd.Validate(); err != nil {
-		if !base.Match(err, ErrNonCurrencyCode) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := nd.Validate()
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonCurrencyCode.Error())
 }
 
 // TestAmountNegotiatedDiscountAmountRequired validates AmountNegotiatedDiscount Amount is required
 func TestAmountNegotiatedDiscountRequired(t *testing.T) {
 	nd := mockAmountNegotiatedDiscount()
 	nd.RemittanceAmount.Amount = ""
-	if err := nd.Validate(); err != nil {
-		if !base.Match(err, ErrFieldRequired) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := nd.Validate()
+
+	require.NotNil(t, err)
+	require.Equal(t, fieldError("Amount", ErrFieldRequired).Error(), err.Error())
 }
 
 // TestAmountNegotiatedDiscountCurrencyCodeRequired validates AmountNegotiatedDiscount CurrencyCode is required
 func TestAmountNegotiatedDiscountCurrencyCodeRequired(t *testing.T) {
 	nd := mockAmountNegotiatedDiscount()
 	nd.RemittanceAmount.CurrencyCode = ""
-	if err := nd.Validate(); err != nil {
-		if !base.Match(err, ErrFieldRequired) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := nd.Validate()
+
+	require.NotNil(t, err)
+	require.Equal(t, fieldError("CurrencyCode", ErrFieldRequired).Error(), err.Error())
 }
 
 // TestParseAmountNegotiatedDiscountWrongLength parses a wrong AmountNegotiatedDiscount record length
@@ -71,15 +71,11 @@ func TestParseAmountNegotiatedDiscountWrongLength(t *testing.T) {
 	var line = "{8550}USD1234.56          "
 	r := NewReader(strings.NewReader(line))
 	r.line = line
-	fwm := new(FEDWireMessage)
-	nd := mockAmountNegotiatedDiscount()
-	fwm.SetAmountNegotiatedDiscount(nd)
+
 	err := r.parseAmountNegotiatedDiscount()
-	if err != nil {
-		if !base.Match(err, NewTagWrongLengthErr(28, len(r.line))) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), NewTagWrongLengthErr(28, len(r.line)).Error())
 }
 
 // TestParseAmountNegotiatedDiscountReaderParseError parses a wrong AmountNegotiatedDiscount reader parse error
@@ -87,30 +83,25 @@ func TestParseAmountNegotiatedDiscountReaderParseError(t *testing.T) {
 	var line = "{8550}USD1234.56Z           "
 	r := NewReader(strings.NewReader(line))
 	r.line = line
-	fwm := new(FEDWireMessage)
-	nd := mockAmountNegotiatedDiscount()
-	fwm.SetAmountNegotiatedDiscount(nd)
+
 	err := r.parseAmountNegotiatedDiscount()
-	if err != nil {
-		if !base.Match(err, ErrNonAmount) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAmount.Error())
+
 	_, err = r.Read()
-	if err != nil {
-		if !base.Has(err, ErrNonAmount) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), ErrNonAmount.Error())
 }
 
 // TestAmountNegotiatedDiscountTagError validates AmountNegotiatedDiscount tag
 func TestAmountNegotiatedDiscountTagError(t *testing.T) {
 	nd := mockAmountNegotiatedDiscount()
 	nd.tag = "{9999}"
-	if err := nd.Validate(); err != nil {
-		if !base.Match(err, ErrValidTagForType) {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
+
+	err := nd.Validate()
+
+	require.NotNil(t, err)
+	require.Equal(t, fieldError("tag", ErrValidTagForType, nd.tag).Error(), err.Error())
 }
