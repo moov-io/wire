8a6809d484bfb06d359cc14747a62a1583de1692
Jose <mfdeveloper508@gmail.com>
2022-05-12 02:50:07 -0500

    change error string

--- fileErrors.go
+++ fileErrors.go
@@ -24,7 +24,7 @@ type TagWrongLengthErr struct {
 // NewTagWrongLengthErr creates a new error of the TagWrongLengthErr type
 func NewTagWrongLengthErr(tagLength, length int) TagWrongLengthErr {
 	return TagWrongLengthErr{
-		Message:   fmt.Sprintf("must be %d characters and found %d", tagLength, length),
+		Message:   fmt.Sprintf("must be minimum %d characters and found %d", tagLength, length),
 		TagLength: tagLength,
 		Length:    length,
 	}
